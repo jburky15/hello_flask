9428ee0245799e97a8901e0405c7e56a226cc553
jburky15 <jburky15@gmail.com>
2023-08-07 23:57:31 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask (__name__)
+
+app.secret_key = "ngeagtahatg3q"
+
+@app.route("/")
+
+def index():
+    flash("Hello, what is your name?")
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hi " + str(request.form['name_input']) + " great to meet you!")
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
