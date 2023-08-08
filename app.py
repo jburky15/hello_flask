56c0069dbc1b0443bb007233c3c7331547b7c461
jburky15 <jburky15@gmail.com>
2023-08-08 09:52:15 -0400

    Added comments, because I'm anal like that sometimes

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ app.secret_key = "ngeagtahatg3q"
 
 @app.route("/hello")
 
+# Render the initial page
 def index():
     flash("Hello, what is your name?")
     return render_template("index.html")
 
+# Re-route the user once the have entered their name and pressed the "Greet" button
 @app.route("/greet", methods=["POST", "GET"])
 def greet():
     flash("Hi " + str(request.form['name_input']) + " great to meet you!")
     return render_template("index.html")
 
+# For testing only
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
